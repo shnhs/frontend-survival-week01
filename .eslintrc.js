0c59742e2e7b02dc90fb85be451ec65f0a0cac7a
hsshin <heesup.shin@saltlux.com>
2025-06-16 15:44:47 +0900

    [fix] prettier 충돌 해결을 위한 패키지 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,34 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		jest: true,
-	},
-	extends: [
-		'xo',
-		'plugin:react/recommended',
-		'plugin:react/jsx-runtime',
-	],
-	overrides: [
-		{
-			env: {
-				node: true,
-			},
-			files: [
-				'.eslintrc.{js,cjs}',
-			],
-			parserOptions: {
-				sourceType: 'script',
-			},
-		},
-		{
-			extends: [
-				'xo-typescript',
-			],
-			files: [
-				'*.ts',
-				'*.tsx',
-			],
-		},
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: [
-		'react',
-	],
-	rules: {'react/react-in-jsx-scope': 'off',
-	},
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'xo',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+    {
+      extends: ['xo-typescript', 'prettier'],
+      files: ['*.ts', '*.tsx'],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: { 'react/react-in-jsx-scope': 'off' },
 };
